ebac0dea56ee9d874adb6147879747250f2610a7
Muhammad Ahsan Ayaz <ahsan.ubitian@gmail.com>
2022-10-23 19:02:18 +0200

    added basic routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,28 @@
 import type { Component } from 'solid-js';
 import { Router, Routes, Route, hashIntegration } from 'solid-app-router';
-
-import logo from './logo.svg';
-import styles from './App.module.css';
+import About from './pages/About';
+import Home from './pages/Home';
 import Navbar from './NavBar';
 
 const App: Component = () => {
   return (
     <Router source={hashIntegration()}>
-      <div class={styles.App}>
-        <Navbar />
+      <div class="flex flex-col h-full min-h-screen">
+        <Navbar></Navbar>
+        <main class="px-8 py-4 flex-1 flex flex-col h-full">
+          <Routes>
+            <Route path="/about" element={<About />} />
+            <Route path="/" element={<Home />}>
+              {/* <Route path="/" element={<RestClientIndex />} />
+              <Route
+                path="/:id"
+                element={<RestClient />}
+                data={fetchSelectedRequest}
+              /> */}
+            </Route>
+          </Routes>
+        </main>
       </div>
-      <Routes>
-        <Route path="/" component={() => <div>Test</div>}></Route>
-        <Route path="/about" component={() => <div>About</div>}></Route>
-      </Routes>
     </Router>
   );
 };
