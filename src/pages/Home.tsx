3b241cb1681170dcb64fb276a125b757b55d418a
Muhammad Ahsan Ayaz <ahsan.ubitian@gmail.com>
2022-10-31 14:17:18 +0100

    add feature request modal

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,69 +1,65 @@
-import { Outlet, Link } from 'solid-app-router';
-import { Component, For } from 'solid-js';
-import { IRestRequest } from '../interfaces/rest.interfaces';
+import { Outlet, Link, useNavigate, useLocation } from "solid-app-router";
+import { Component, createSignal, For } from "solid-js";
+import IconButton from "../components/IconButton";
+import RequestModal from "../components/RequestModal";
+import { IRestRequest } from "../interfaces/rest.interfaces";
+import { restRequests } from "../store";
 
 const Home: Component = () => {
-  const requests: IRestRequest[] = [
-    {
-      id: "1",
-      name: "Get Scores",
-      description: "Getting scores from server",
-      request: {
-        method: "GET",
-        url: "https://scorer-pro3.p.rapidapi.com/score/game123",
-        headers: [
-          {
-            key: "X-RapidAPI-Host",
-            value: "API_HOST_FROM_RAPID_API",
-          },
-          {
-            key: "X-RapidAPI-Key",
-            value: "API_KEY_FROM_RAPID_API",
-          },
-        ],
-      },
-    },
-    {
-      id: "2",
-      name: "Add Score",
-      description: "Adding scores to server",
-      request: {
-        method: "POST",
-        url: "https://scorer-pro3.p.rapidapi.com/score",
-        headers: [
-          {
-            key: "X-RapidAPI-Host",
-            value: "API_HOST_FROM_RAPID_API",
-          },
-          {
-            key: "X-RapidAPI-Key",
-            value: "API_KEY_FROM_RAPID_API",
-          },
-        ],
-        body: JSON.stringify({
-          score: 100,
-          gameId: "123",
-          userId: "test123",
-        }),
-      },
-    },
-  ];
+  const [showModal, setShowModal] = createSignal(false);
+  const navigate = useNavigate();
+  const location = useLocation();
   return (
-    <div class="flex flex-col md:flex-row gap-4 h-full flex-1">
-      <div class="w-full md:w-1/4 bg-gray-200 min-h-full border-gray-300 border p-4 rounded-lg">
-        <div class="flex justify-between py-4">
-          <h1 class="text-sm ">Rest Requests</h1>
-          <button class="flex hover:bg-opacity-60 justify-center items-center p-4 bg-purple-600 rounded-full text-white w-8 h-8" onClick={() => alert('To be implemented')}>
-            <div>+</div>
-          </button>
+    <>
+      <div onClick={() => setShowModal(!showModal())}>
+        <RequestModal
+          show={showModal()}
+          onModalHide={(id: string | null) => {
+            setShowModal(!showModal());
+            if (id) {
+              navigate(`/${id}`);
+            }
+          }}
+        />
+      </div>
+      <div class="flex flex-col md:flex-row gap-4 h-full flex-1">
+        <div class="w-full md:w-1/4 bg-gray-200 min-h-full border-gray-300 border p-4 rounded-lg">
+          <div class="flex justify-between py-4">
+            <h1 class="text-sm ">Rest Requests</h1>
+            <IconButton
+              onClick={() => setShowModal(true)}
+              icon="add"
+              label="Add Request"
+            />
+          </div>
+          <div class="list">
+            <For each={restRequests()} fallback={<div>Loading...</div>}>
+              {(item) => (
+                <Link href={`/${item.id}`} class="relative list__item">
+                  <div
+                    class="p-2 hover:bg-gray-300 cursor-pointer pr-12 rounded-lg mb-2"
+                    classList={{
+                      "list__item--active": Boolean(
+                        location.pathname === `/${item.id}`
+                      ),
+                    }}
+                  >
+                    <div>{item.name}</div>
+                    <div class="text-xs break-all">
+                      {item.request.method} {item.request.url}
+                    </div>
+                  </div>
+                </Link>
+              )}
+            </For>
+          </div>
         </div>
-        
+        <div class="flex-1 min-h-full">
+          <Outlet />
+        </div>
       </div>
-      <div class="flex-1 min-h-full">
-        <Outlet />
-      </div>
-    </div>
-  )
-}
+    </>
+  );
+};
 
 export default Home;
